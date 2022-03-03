a852c8dd827494e9c7f0a8148439efa935b4a676
AMINEWAP <100165297+AMINEWAP@users.noreply.github.com>
2022-03-03 01:31:47 -0600

    Add files via upload

---
+++ login.js
@@ -0,0 +1,10 @@
+function handleSubmit() {
+    console.log("Lab 04 ===================== ");
+    let form = document.getElementById("loginForm");
+    console.log("Email: " + form.elements['email'].value);
+    console.log("Password: " + form.elements['password'].value);
+    console.log("Website URL: " + form.elements['webUrl'].value);
+    console.log("Check me out: " + document.getElementById("loginForm").elements['checkMeOut'].checked);
+    
+    return false;
+}
